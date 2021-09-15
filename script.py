bcdac03ea736e0114f067c383d7923c05e141d51
roberto <robertocarta.mail@gmail.com>
2021-09-15 11:29:49 +0200

    Keep all data in memory

--- script.py
+++ script.py
@@ -1,17 +1,22 @@
-import os
-import os.path
 import requests
+import zipfile 
+import requests
+from io import BytesIO
+
+u = requests.get("http://www.pythonchallenge.com/pc/def/channel.zip")
+
+def extract_zip(input_zip):
+    input_zip = zipfile.ZipFile(input_zip)
+    return {i: input_zip.read(i).decode() for i in input_zip.namelist()}
 
 
-def DownloadSubs(serie_name, seasons_number):
-    serie = serie_name.replace(" ", "%20")
-    for season in range(1, seasons_number+1):
-        if os.path.isfile(f'{serie_name}-season-{season}.zip'): 
-        season_n = season
+def DownloadSubs(series_name, season_number):
+    serie = series_name.replace(" ", "%20")
+    for season_n in range(1, season_number + 1):
         url = f"http://www.tvsubtitles.net/files/seasons/{serie}%20-%20season%20{season_n}.en.zip"
-        
         req = requests.get(url, allow_redirects=True)
-        open(f'{serie_name}-season-{season}.zip', 'wb').write(req.content)
+        f = BytesIO(req.content)
+        files = extract_zip(f)
 
 
 DownloadSubs("Lost", 4)
