2b129463e8a58c757cf08089f4e7d66f570e9ba2
roberto <robertocarta.mail@gmail.com>
2021-09-15 23:57:08 +0200

    Lemmatize working

--- script.py
+++ script.py
@@ -5,7 +5,10 @@ import zipfile
 import pysrt
 import nltk
 from nltk import word_tokenize as tok
-import enchant
+from nltk.stem import PorterStemmer, WordNetLemmatizer
+from nltk.corpus import wordnet, stopwords
+from collections import defaultdict
+from pprint import pprint
 
 def extract_zip(input_zip, directory):
     zip_file = zipfile.ZipFile(input_zip)
@@ -20,7 +23,9 @@ def DownloadSubs(serie_name, seasons_number):
         season_n = season
         url = f"http://www.tvsubtitles.net/files/seasons/{serie}%20-%20season%20{season_n}.en.zip"
         
+        print(url)
         req = requests.get(url, allow_redirects=True)
+        print('done')
         open(f'{serie_name}-season-{season}.zip', 'wb').write(req.content)
 
 
@@ -54,25 +59,17 @@ def srt_total(directory):
     return totaltext
 
 def token_dict(list_tokens):
-    D = {}
-    d_us = enchant.Dict("en_US")
-    d_gb = enchant.Dict("en_GB")
+    D = defaultdict(int)
     for word in list_tokens:
-        
-        if d_us.check(word) or d_gb.check(word):
-            if word in D:
-                D[word] += 1
-            else:
-                D[word] = 1
-        else:
-            next
+        if is_english(word):
+            D[word] += 1
     return D
 
 def sort_dic(dictionary):
     #copiata da SO, non capisco come funzioni
     sorted_d = sorted(dictionary.items(), key=lambda kv: kv[1])
-    rev_sorted = sorted_d[::-1]
-    return rev_sorted
+    # rev_sorted = sorted_d[::-1]
+    return sorted_d
 
 
 def main(serie, seasons):
@@ -84,11 +81,40 @@ def main(serie, seasons):
     
     return sorted_list
 
+def is_english(w):
+    if wordnet.synsets(w):
+        return True
+    return False
+
+
+
+# [ (instance, True) ]
+def lemmatize(w, lemmatizers):
+    r = w
+    for lemmatizer, check_dict in lemmatizers:
+        temp = lemmatizer(w)
+        if check_dict:
+            if is_english(temp):
+                r = temp
+        else:
+            r = temp
+    return r
+
+
+
+
+
 #if files already downloaded, skip a passage
 def main_quick(serie):
     text = srt_total(f"./{serie}")
     List_tokens = tok(text)
-    dict_tok = token_dict(List_tokens)
+
+    stemmers = [ (WordNetLemmatizer().lemmatize, False), (PorterStemmer().stem, True) ]
+    stemmed_tokens = [lemmatize(w, stemmers) for w in List_tokens]
+    english_stopwords = stopwords.words('english')
+    interesting_words = [w for w in stemmed_tokens if w not in english_stopwords]
+
+    dict_tok = token_dict(interesting_words)
     print(type(dict_tok))
     sorted_list = sort_dic(dict_tok)
     print(type(sorted_list))
@@ -98,9 +124,13 @@ def main_quick(serie):
 ###The function doesn't check if the zip files have already been extracted
 ###Until a checker is added, remember to cancel the folder
 
-result = main_quick("Lost")
+if __name__ == '__main__':
+    result = main_quick("Lost")
 
-print(f'The type of the output is {type(result)}')
-print(f'The number of items in the output is {len(result)}')
-print(result)
+    print(f'The type of the output is {type(result)}')
+    print(f'The number of items in the output is {len(result)}')
+    pprint(result)
+
+# Choose between season/episode(s)
+
 
